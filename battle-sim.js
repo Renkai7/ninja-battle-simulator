122e31d7824152993e6745e3dda02f1fbae93a84
Michael Spencer <ms53150@gmail.com>
2023-11-20 18:51:34 -0500

    add loop for battle and allow user to choose character to attack with

--- battle-sim.js
+++ battle-sim.js
@@ -10,10 +10,6 @@ const Sasuke = {
 	attackPoints: 50,
 };
 
-const userCharacterChoice = prompt(
-	"Choose a character: Naruto (press '1') | Sasuke (press '2')"
-);
-
 const battle = (attacker, defender) => {
 	const damage = attacker.attackPoints;
 	defender.healthPoints -= damage;
@@ -25,13 +21,49 @@ const battle = (attacker, defender) => {
 	console.log(
 		`${attacker.name} attacked ${defender.name} for ${damage} points. ${defender.name} has ${defender.healthPoints} left.`
 	);
-	detetemineWinner(attacker, defender);
 };
 
-const detetemineWinner = (userOne, userTwo) => {
-	if (userOne.healthPoints === 0) {
-		console.log("User one loses!");
-	} else if (userTwo.healthPoints === 0) {
-		console.log("User two loses!");
+const detetemineWinner = () => {
+	if (Naruto.healthPoints === 0) {
+		console.log(`${Sasuke.name} wins!`);
+		return true;
+	} else if (Sasuke.healthPoints === 0) {
+		console.log(`${Naruto.name} wins!`);
+		return true;
+	} else {
+		return false;
 	}
 };
+
+const main = () => {
+	while (true) {
+		let currentUserCharacter = "";
+		const userCharacterChoice = Number(
+			prompt("Choose a character: Naruto (press '1') | Sasuke (press '2')")
+		);
+
+		switch (userCharacterChoice) {
+			case 1:
+				currentUserCharacter = Naruto.name;
+				console.log(`You chose ${Naruto.name}!`);
+				battle(Naruto, Sasuke);
+				break;
+			case 2:
+				currentUserCharacter = Sasuke.name;
+				console.log(`You chose ${Sasuke.name}!`);
+				battle(Sasuke, Naruto);
+				break;
+			case 3:
+				console.log("Thanks for playing!");
+				return;
+			default:
+				console.log("Please enter 1, 2, or 3.");
+		}
+
+		if (detetemineWinner()) {
+			return;
+		}
+	}
+};
+
+main();
