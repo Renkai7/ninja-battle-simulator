d68ce1323e93d295a7eeaf8343a207ddeccc17ee
Michael Spencer <ms53150@gmail.com>
2023-11-20 14:24:36 -0500

    add battle logic to deal damage to opponent

--- battle-sim.js
+++ battle-sim.js
@@ -13,3 +13,11 @@ const Sasuke = {
 const userCharacterChoice = prompt(
 	"Choose a character: Naruto (press '1') | Sasuke (press '2')"
 );
+
+const battle = (attacker, defender) => {
+	const damage = attacker.attackPoints;
+	defender.healthPoints -= damage;
+	console.log(
+		`${attacker.name} attacked ${defender.name} for ${damage} points. ${defender.name} has ${defender.healthPoints} left.`
+	);
+};
