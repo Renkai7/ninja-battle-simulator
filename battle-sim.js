a81004ff43fb046d819889cb5822a5356c761a46
Michael Spencer <ms53150@gmail.com>
2023-11-20 14:29:31 -0500

    add logic to determine winner

--- battle-sim.js
+++ battle-sim.js
@@ -17,7 +17,21 @@ const userCharacterChoice = prompt(
 const battle = (attacker, defender) => {
 	const damage = attacker.attackPoints;
 	defender.healthPoints -= damage;
+
+	// prevent health going below 0
+	if (defender.healthPoints < 0) {
+		defender.healthPoints = 0;
+	}
 	console.log(
 		`${attacker.name} attacked ${defender.name} for ${damage} points. ${defender.name} has ${defender.healthPoints} left.`
 	);
+	detetemineWinner(attacker, defender);
+};
+
+const detetemineWinner = (userOne, userTwo) => {
+	if (userOne.healthPoints === 0) {
+		console.log("User one loses!");
+	} else if (userTwo.healthPoints === 0) {
+		console.log("User two loses!");
+	}
 };
